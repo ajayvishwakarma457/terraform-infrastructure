3182cdc1f2222b87683356df7edc24fa80acc488
Ajay <ajayvishwakarma457@gmail.com>
2025-12-16 23:54:27 +0530

    changes

--- all-in-one/main.tf
+++ all-in-one/main.tf
@@ -120,17 +120,11 @@ module "s3_app" {
 module "lightsail_web" {
   source = "./modules/compute/lightsail"
 
-  providers = {
-    aws      = aws
-    aws.use1 = aws.use1
-  }
-
   name              = "web-dev"
   availability_zone = "ap-south-1a"
   bundle_id         = "nano_3_1"
   create_snapshot   = true 
   disk_size_gb = 50
-  domain_name = var.domain_name
 
   tags = {
     Client = "spakcommgroup"
