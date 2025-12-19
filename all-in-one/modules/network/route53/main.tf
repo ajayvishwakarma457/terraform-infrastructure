69196869d87a1460008c4a8523c60bf8b2ab2f70
Ajay Spak <ajayspakcommcom>
2025-12-19 11:45:08 +0530

    changes

--- all-in-one/modules/network/route53/main.tf
+++ all-in-one/modules/network/route53/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_route53_zone" "this" {
   count = var.create_zone ? 1 : 0
   name  = var.zone_name
@@ -10,12 +11,12 @@ locals {
 
 resource "aws_route53_record" "this" {
   for_each = {
-    for r in var.records :
-    "${r.name}-${r.type}" => r
+    # for r in var.records : "${r.name}-${r.type}" => r
+    for idx, r in var.records : "${idx}-${r.name}-${r.type}" => r
   }
 
   zone_id = local.hosted_zone_id
-  name    = each.value.name
+  name    = each.value.name == "@" ? var.zone_name : each.value.name
   type    = each.value.type
   ttl     = each.value.ttl
   records = each.value.records
