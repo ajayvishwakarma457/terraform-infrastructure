835d5d4ca1dd7dcca7f816b75ef2ae49f1b918d2
Ajay Spak <ajayspakcommcom>
2025-12-24 12:15:52 +0530

    updated

--- all-in-one/modules/security/iam/main.tf
+++ all-in-one/modules/security/iam/main.tf
@@ -99,7 +99,6 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2_role.name
 }
 
-
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_policy" "rds_iam_auth" {
@@ -115,4 +114,80 @@ resource "aws_iam_policy" "rds_iam_auth" {
       }
     ]
   })
+}
+
+
+resource "aws_iam_policy" "ecr_pull" {
+  name = "ecr-pull-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecr_push" {
+  name = "ecr-push-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "github_actions_ecr" {
+  name = "github-actions-ecr"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:ORG/REPO:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_ecr_push" {
+  role       = aws_iam_role.github_actions_ecr.name
+  policy_arn = aws_iam_policy.ecr_push.arn
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
