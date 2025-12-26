c0bbd07b496666b5ca20f8f2e8dc544d7e704f6f
Ajay Spak <ajayspakcommcom>
2025-12-26 19:16:09 +0530

    updated

--- all-in-one/modules/security/secret/main.tf
+++ all-in-one/modules/security/secret/main.tf
@@ -1,13 +1,11 @@
 resource "aws_secretsmanager_secret" "this" {
   name        = var.name
   description = var.description
-
   tags = var.tags
 }
 
 resource "aws_secretsmanager_secret_version" "this" {
   secret_id = aws_secretsmanager_secret.this.id
-
   secret_string = jsonencode(var.secret_value)
 }
 
