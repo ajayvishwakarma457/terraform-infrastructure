4033e0aad25c5c722ba07fe1304912a94c79a79c
Ajay Spak <ajayspakcommcom>
2025-12-29 14:16:29 +0530

    updated

--- all-in-one/modules/security/iam/main.tf
+++ all-in-one/modules/security/iam/main.tf
@@ -224,7 +224,8 @@ resource "aws_iam_policy" "ecs_secrets" {
       Action = [
         "secretsmanager:GetSecretValue"
       ]
-      Resource = var.secret_arn
+      # Resource = var.secret_arn
+      Resource = "${var.secret_arn}*"
     }]
   })
 }
@@ -262,4 +263,9 @@ resource "aws_iam_role" "ecs_task_role" {
       Action = "sts:AssumeRole"
     }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "execution" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
