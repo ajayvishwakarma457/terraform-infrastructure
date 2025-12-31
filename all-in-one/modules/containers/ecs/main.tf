3e906401e66a58fbcb7643ceada17041512b44eb
Ajay Spak <ajayspakcommcom>
2025-12-31 15:59:11 +0530

    updated

--- all-in-one/modules/containers/ecs/main.tf
+++ all-in-one/modules/containers/ecs/main.tf
@@ -112,9 +112,55 @@ resource "aws_ecs_service" "this" {
 
   # depends_on = [aws_iam_role_policy_attachment.execution]
   depends_on = [var.ecs_execution_policy_attached_id]
-
-  
 }
 
 
+
+resource "aws_appautoscaling_target" "ecs" {
+  service_namespace  = "ecs"
+  resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.this.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+
+  min_capacity = 1
+  max_capacity = 5
+
+  depends_on = [aws_ecs_service.this]
+}
+
+resource "aws_appautoscaling_policy" "cpu_scale_out" {
+  name               = "${var.service_name}-cpu-scale-out"
+  policy_type        = "TargetTrackingScaling"
+  service_namespace  = aws_appautoscaling_target.ecs.service_namespace
+  resource_id        = aws_appautoscaling_target.ecs.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = 70
+    scale_out_cooldown = 60
+    scale_in_cooldown  = 120
+  }
+}
+
+
+resource "aws_appautoscaling_policy" "memory_scale" {
+  name               = "${var.service_name}-memory-scale"
+  policy_type        = "TargetTrackingScaling"
+  service_namespace  = aws_appautoscaling_target.ecs.service_namespace
+  resource_id        = aws_appautoscaling_target.ecs.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = 75
+    scale_out_cooldown = 60
+    scale_in_cooldown  = 120
+  }
+}
 
